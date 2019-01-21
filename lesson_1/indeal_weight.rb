e290aa422990d586f8e29b09c1f1f83dd6cedcbd
Ruslan Knyazev <agrry@yandex.ru>
2019-01-21 17:24:51 +0500

    ideal weight added

---
+++ lesson_1/indeal_weight.rb
@@ -0,0 +1,13 @@
+puts 'Введите Ваше имя'
+name = gets.chomp
+
+puts 'Введите Ваш рост'
+height = gets.chomp.to_i
+
+ideal_weight = height - 110
+
+if (ideal_weight > 0)
+  puts "#{name} Ваш идеальны вес: #{ideal_weight}";
+else
+  puts "Ваш вес уже оптимальный"
+end
